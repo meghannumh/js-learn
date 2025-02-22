c374e503e6a84d7879acc1d9467a3d4455d6973f
meghannumh <61019674+meghannumh@users.noreply.github.com>
2025-02-22 13:53:16 +0000

    Conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "33";
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+let isLoggedIn = 1;
+isLoggedIn = Boolean(isLoggedIn);
+
+// console.log(isLoggedIn);
+
+//1 => true; 0=> false;
+//"" => false;
+//"meghana"=>true
+
+
+
