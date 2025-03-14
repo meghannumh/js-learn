33b94cba6db2b3b04cfc781368e5f9dd547bbc8a
meghannumh <61019674+meghannumh@users.noreply.github.com>
2025-03-14 14:28:10 +0000

    scope level & mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,7 +6,7 @@ if(true){
 
 // console.log(a); //gives a not defined
 // console.log(b); // gives b not defined
-console.log(c); // prints 30
+// console.log(c); // prints 30
 
 var c = 300
 let a = 300
@@ -16,8 +16,49 @@ if(true){
     const b = 20;
     var c = 30;
 
-    console.log("INNER: ", a) // prints 10 because of block scope
+    // console.log("INNER: ", a) // prints 10 because of block scope
 }
 
-console.log(a) // prints 300 becuase of global scope
-console.log(c) // prints 30
+// console.log(a) // prints 300 becuase of global scope
+// console.log(c) // prints 30
+
+
+function one(){
+    const username = "meghana";
+
+    function two(){
+        const website = "youtube";
+        console.log(username); // prints meghana
+    }
+    // console.log(website) // gives error website not defined
+
+    two();
+}
+
+one();
+
+if(true){
+    const username = "meghana";
+    if(username === "meghana"){
+        const website = " youtube";
+        console.log(username + website);
+    }
+}
+
+
+
+// ++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+
+console.log(addone(5)) // prints 6
+
+function addone(num){
+    return num + 1;
+}
+
+
+console.log(addTwo(5)) // cannot access 'addTwo' before intialization
+
+const addTwo = function(num){
+    return num + 2;
+}
