0ce2bbaad0bd04f2373510f5289eeaa80ca98446
meghannumh <61019674+meghannumh@users.noreply.github.com>
2025-03-15 07:02:05 +0000

    this and arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,10 +3,50 @@ const user = {
     price: 999,
 
     welcomeMessage: function(){
-        console.log(`${this.usesrname}, welcome to website`)
+        console.log(`${this.usesrname}, welcome to website`);
+        // console.log(this);
     }
 }
 
-user.welcomeMessage() // prints => meghana, welcome to website
-user.usesrname = "sam";
-user.welcomeMessage() // prints => sam, welcome to website
+// user.welcomeMessage() // prints => meghana, welcome to website
+// user.usesrname = "sam";
+// user.welcomeMessage() // prints => sam, welcome to website
+
+// console.log(this) //prints {}
+
+
+// function chai(){
+//     let usesrname = "meghana"
+//     console.log(this.usesrname); //  prints undefined
+// }
+
+// chai() //prints many values
+
+// const chai = function () {
+//     let usesrname = "meghana"
+//     console.log(this.usesrname)
+// }
+
+// chai()
+
+
+const chai = () => {
+    let usesrname = "meghana"
+    console.log(this.usesrname) //in arrow fucntin we do not use this whereas in regular function we ca=n
+}
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(addTwo(3, 4)) //prints 7
+
+// const addTwo = (num1, num2) =>  num1 + num2; //implicit return
+
+// console.log(addTwo(3, 4)) //prints  7
+const addTwo = (num1, num2) =>  (num1 + num2); //implicit return
+
+console.log(addTwo(3, 4)) //prints  7
+
+
+
